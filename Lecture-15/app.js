e110f3fd05e0d1b948e566efbfd047c1438b8b0f
Vaibhav Desai <vaidesai@ebay.com>
2015-12-17 22:57:34 -0800

    added gitognore and lec15

---
+++ Lecture-15/app.js
@@ -0,0 +1,11 @@
+
+var a = 1;
+var b = 2;
+var c = a + b;
+
+// This will print on console in browser
+// Node guys were nice enough to create the same function
+// to print to stdout since people are used to console.log(...)
+console.log(c);
+
+
